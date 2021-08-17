2834a0a702d2b719808b6ac5d436fbbdc619a2de
Erick A. I. S <76917713+erick-ais@users.noreply.github.com>
2021-08-17 09:32:07 -0300

    Primeiro Programa Dart

---
+++ Fundamentos/olamundo.dart
@@ -0,0 +1,13 @@
+main() {
+  print("Olá, Mundo!");
+
+  /*
+    OBSERVAÇÕES:
+      Linguagem fortemente tipada;
+      Os blocos de códigos são definidos por chaves {};
+      Multiparadigma (suporta mais de um paradigma de programação);
+      Baseada em clsses e objetos (POO);
+      A sintaxe é baseada na linguagem C;
+      Usada com o framework flutter para o desenvolvimento de interface de usuário.
+  */
+}
